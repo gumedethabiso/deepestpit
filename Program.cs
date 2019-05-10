f019bdaf09c915733529cb40db4dfd22e069d037
gumedethabiso <50336770+gumedethabiso@users.noreply.github.com>
2019-05-10 22:40:55 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -22,29 +22,93 @@ namespace Question2
                 return -1;
             }
 
-            int P = inx[0], Q = P, R;                                  /*initializing the points of the depth of the pit */
+            int P = inx[0], Q = P, R;                                  /*declaring the points of the depth of the pit */
 
             int down = -1;                                             /* this is the depth from the highest point*/
             int upward = -1;                                           /* the lengty from the bottok of the pit*/
 
-            for (int x = 0; x < points.Length; x++ )                   
-            {                 
+            for (int x = 0; x < points.Length; x++)
+            {
                 if (x + 1 >= points.Length)                            /* this loop prevents the occurance of an error when the slope reaches the lowest point */
                 {
-                    return upward;          
+                    return upward;
+                }
+
+                if (P < points[x + 1] && !R.HasValue)                        /* condition to check "p", the highest point on the slope*/
+                {
+                    P = points[x + 1];                                  /* setting the first or new highest point */
+                    Q = points[x + 1];                                  /* setting the lowest or second point, "q" */
+                    continue;                                           /* exits the loop earlier */
+                }
+
+                if (Q > points[x + 1])                                       /*condition to check "q", the lowest point or the second point*/
+                {
+                    Q = points[x + 1];
+                    R = points[x + 1];
+                    continue;
+                }
+
+                if (R < points[x + 1])                                       /* now that we have the first and second point, the next one is the third*/
+                {
+                    R = points[x + 1];
+
+                    down = CalculateDepth(P.Value, Q.Value, R.Value);      /* getting the points to fet the values*/
+                    upward = CompareDepths(upward, down);
+
+
+                    //if it is set P to value of this so we can work out next slope
+                    if ((x + 2) < points.Length && !(R < points[x + 2]))             /* checking if this is the highest point or  there is another element in the array */
+                    {
+                        P = R;
+                    }
+                }
+                else
+                {
+                    P = R ?? P;                                                      /* setting the next point to start from  the previous slopes "p" */
+                    Q = points[x + 1];                                               /* reseting the current slope "Q" to previous slopes' "p" */
+                    R = null;                                                        /* not assigning a value to the"r" of the slope */
+                }
+
+                if (x + 2 == points.Length)                                          /* get the length of upward */
+                {
+                    return upward;
                 }
             }
+            return upward;                                                           /* returns if nothing runs as default "-1" */
+        }
+        public int CalculateDepth(int P, int Q, int R)
+        {
+            var outcome1 = P - Q;
+            var outcome2 = R - Q;
 
-            public bool HasTriplet(int[] point)                             /*Function to check if there are 3 integers in the array*/
+            if (outcome1 < outcome2)
             {
-                if (A.Length >= 3)
-                {
-                    return true;
-                }
-
-                return false;
+                return outcome1;
+            }
+            else
+            {
+                return outcome2;
             }
         }
 
+        public int CompareDepths(int upward, int down)
+        {
+            if (down > upward)
+            {
+                return down;
+            }
+
+            return upward;
+        }
+        public bool HasTriplet(int[] point)                             /*Function to check if there are 3 integers in the array*/
+        {
+            if (A.Length >= 3)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
+     
